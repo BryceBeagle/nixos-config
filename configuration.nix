440647fa95a820f8f714dfc5c9071243c2b84dfb
ignormies <bryce.beagle@gmail.com>
2024-12-21 14:15:22 -0800

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,253 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz";
+  impermanence = builtins.fetchTarball "https://github.com/nix-community/impermanence/archive/master.tar.gz";
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+     (import "${home-manager}/nixos")
+      "${impermanence}/nixos.nix"
+    ]; 
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # persist some system directories
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = [
+      "/var/log"
+      "/var/lib/nixos"
+      "/var/lib/alsa"
+      "/var/lib/bluetooth"
+      "/var/lib/systemd/coredump"
+      "/etc/NetworkManager/system-connections"
+    ];
+  };
+
+
+  networking.hostName = "poundcake"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+    # useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.excludePackages = [ pkgs.xterm ];
+  services.gnome.core-utilities.enable = false;
+  environment.gnome.excludePackages = with pkgs; [
+    gnome.gnome-shell-extensions
+    gnome-tour
+  ];
+  
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable sound.
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    curl
+    just
+    neovim
+    tree
+    wget
+
+    # Gnome tools
+    loupe
+    gedit
+    gnome.baobab
+    gnome.gnome-calculator
+    gnome.gnome-screenshot
+    gnome.gnome-system-monitor
+    gnome.gnome-terminal
+    gnome.gnome-tweaks
+    gnome.nautilus
+
+    libinput
+
+    discord
+    spotify
+    timeshift
+  ];
+
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "discord"
+      "spotify"
+    ];
+
+  programs.fuse.userAllowOther = true;  # required for 'home-manager...home.persistence.allowOther = true'
+  programs.git.enable = true;
+
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  environment = {
+    shells = with pkgs; [zsh];
+    variables.EDITOR = "nvim";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ignormies = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    initialPassword = "foobar";
+  };
+  home-manager.users.ignormies = {
+    imports = [
+      "${impermanence}/home-manager.nix"
+    ];
+    home.stateVersion = "24.05";
+    
+
+    # https://support.mozilla.org/en-US/kb/profiles-where-firefox-stores-user-data#w_what-information-is-stored-in-my-profile
+    home.persistence."/persist/home/ignormies" = {
+      directories = [
+        ".ssh"
+        ".mozilla/firefox/default/extensions"
+      ];
+      files = [
+        ".mozilla/firefox/default/cookies.sqlite"
+        ".mozilla/firefox/default/favicons.sqlite"
+        # Permissions and zoom levels for each site
+        ".mozilla/firefox/default/permissions.sqlite"
+        ".mozilla/firefox/default/content-prefs.sqlite"
+        # Browser history and bookmarks
+        ".mozilla/firefox/default/places.sqlite"
+        # Session tabs and windows
+        ".mozilla/firefox/default/sessionstore.jsonlz4"
+        # I guess this is useful? 
+        # https://bugzilla.mozilla.org/show_bug.cgi?id=1511384
+        # https://developer.mozilla.org/en-US/docs/Web/API/Storage_API/Storage_quotas_and_eviction_criteria
+        ".mozilla/firefox/default/storage.sqlite"
+        # Window size and positions
+        ".mozilla/firefox/default/xulstore.json"
+      ];
+      allowOther = true;
+    };
+
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;
+    };
+
+    programs.firefox = {
+      enable = true;
+      package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+        extraPolicies = {
+          EnableTrackingProtection = {
+            Value= true;
+            Locked = true;
+            Cryptomining = true;
+            Fingerprinting = true;
+          };
+          ExtensionSettings = {
+	    # Would like to enable, but doing so prevents access to about:debugging
+	    # https://bugzilla.mozilla.org/show_bug.cgi?id=1778559
+	    # 
+	    # "*" = {
+	    #   blocked_install_message = "Modify the NixOS config dingus!";
+	    #   installation_mode = "blocked";
+	    # };
+
+            # https://www.reddit.com/r/firefox/comments/1afja73/how_to_get_an_extensions_name_gpo/
+
+            "addon@darkreader.org" = {
+	      install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+              installation_mode = "force_installed";
+	    };
+	    "uBlock0@raymondhill.net" = {
+	      install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+              installation_mode = "force_installed";
+	    };
+	    "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+	      install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+              installation_mode = "force_installed";
+	    };
+          };
+        };
+      };
+
+      profiles.default = {
+        # https://nix-community.github.io/home-manager/options.xhtml#opt-programs.firefox.profiles._name_.containersForce
+        containersForce = true;
+	# https://nix-community.github.io/home-manager/options.xhtml#opt-programs.firefox.profiles._name_.search.force
+	search.force = true;
+
+	search.engines = {
+
+	  # Hide the other useless search engines from the bottom
+          "Amazon.com".metaData.hidden = true;
+          "Bing".metaData.hidden = true;
+          "DuckDuckGo".metaData.hidden = true;
+          "eBay".metaData.hidden = true;
+          "Wikipedia (en)".metaData.hidden = true;
+	};
+        settings = {
+	  # New tab page junk
+	  "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+	  "browser.newtabpage.activity-stream.feeds.weatherfeed" = false;
+          "browser.newtabpage.activity-stream.showSponsored" = false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+	  "browser.newtabpage.activity-stream.showWeather" = false;
+	  "browser.newtabpage.activity-stream.system.showSponsored" = false;
+	  "browser.newtabpage.activity-stream.system.showWeather" = false;
+	  "browser.newtabpage.activity-stream.weather.locationSearchEnabled" = false;
+
+	  # Search junk
+	  "browser.urlbar.suggest.topsites" = false;
+	  "browser.urlbar.suggest.trending" = false;
+	  "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
+	  "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+	  "browser.urlbar.suggest.yelp" = false;
+
+	  # Addons junk
+	  "extensions.htmlaboutaddons.recommendations.enabled" = false;
+
+	  # Prevent firefox from warning before going to about:config
+	  "browser.aboutConfig.showWarning" = false;
+
+	  # Don't try to store passwords. Using BitWarden for this
+	  "signon.rememberSignons" = false;
+	};
+      };
+    };
+  };
+
+  system.stateVersion = "24.05";
+}
---
+++ hardware-configuration.nix
@@ -0,0 +1,49 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+      options = [ "subvol=@" "compress=zstd" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-label/nixos";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "compress=zstd" "noatime" ];
+    };
+
+  fileSystems."/persist" =
+    { device = "/dev/disk/by-label/nixos";
+      neededForBoot = true;
+      fsType = "btrfs";
+      options = [ "subvol=persist" "compress=zstd" ];
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/BOOT";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp1s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
