47e2d27986150ac13e4ae975f671d1350d0677e7
ignormies <bryce.beagle@gmail.com>
2024-12-28 11:43:33 -0800

    Use tabs instead of spaces in neovim
    Resolves #43

--- configuration.nix
+++ configuration.nix
@@ -224,6 +224,14 @@ in
           '';
         }
       ];
+
+      extraLuaConfig = ''
+        -- https://vi.stackexchange.com/a/28017
+        vim.cmd("set expandtab")
+        vim.cmd("set tabstop=4")
+        vim.cmd("set shiftwidth=4")
+        vim.cmd("set softtabstop=-1")
+      '';
     };
 
     programs.firefox = {
