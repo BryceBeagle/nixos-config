902211118a2b3b9701eda17067b876b94fe0b68b
ignormies <bryce.beagle@gmail.com>
2024-12-27 17:16:35 -0800

    Revert "Disable capslock key"
    This reverts commit 10dfa2e6f260f7929d067124fae5c3dd534e479d.

--- configuration.nix
+++ configuration.nix
@@ -52,22 +52,23 @@ in
     # useXkbConfig = true; # use xkb.options in tty.
   };
 
-  services.xserver = {
-    enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
 
-    excludePackages = [ pkgs.xterm ];
-
-    xkb.options = "ctrl:nocaps";
-  };
-
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.excludePackages = [ pkgs.xterm ];
   services.gnome.core-utilities.enable = false;
   environment.gnome.excludePackages = with pkgs; [
     gnome.gnome-shell-extensions
     gnome-tour
   ];
   
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
   # Enable sound.
   hardware.pulseaudio.enable = false;
   services.pipewire = {
@@ -143,10 +144,6 @@ in
         ".ssh"
         ".mozilla/firefox/default/extensions"
 	"git"
-
-	# Spotify seems to ignore .config is cache is missing
-	".cache/spotify"
-	".config/spotify"
       ];
       files = [
         ".mozilla/firefox/default/cookies.sqlite"
