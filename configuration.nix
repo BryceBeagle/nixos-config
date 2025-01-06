c9b617be70a0063ad5171881c4ebee943f969431
ignormies <bryce.beagle@gmail.com>
2025-01-05 20:53:16 -0800

    Fix Firefox addon ID configuration
    Resolves #63

--- configuration.nix
+++ configuration.nix
@@ -274,11 +274,11 @@
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
               installation_mode = "force_installed";
             };
-            "uBlock0@raymondhill.net" = {
+            "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
               installation_mode = "force_installed";
             };
-            "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            "uBlock0@raymondhill.net" = {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
               installation_mode = "force_installed";
             };
