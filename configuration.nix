BryceBeagle/nixos-config

commit 416dff1260b8844bc1ec11cf5f127d5a537b2031

Use Right Alt (Alt Grave) as compose key
Resolves #64

configuration.nix
@@ -140,7 +140,7 @@
     ];
     initialPassword = "foobar";
   };
-  home-manager.users.ignormies = {
+  home-manager.users.ignormies = { lib, ... }: {
     imports = [
       inputs.impermanence.homeManagerModules.impermanence
     ];
@@ -177,7 +177,11 @@
       allowOther = true;
     };
 
-    dconf.settings = {
+    dconf.settings = with lib.hm.gvariant; {
+      "org/gnome/desktop/input-sources" = {
+        sources = [ (mkTuple [ "xkb" "us" ]) ];
+        xkb-options = [ "compose:ralt" ];
+      };
       "org/gnome/desktop/interface" = {
         color-scheme = "prefer-dark";
       };

commit 498d8ab0af8ffd48e01a34951431718d670e857a

Fix regression with caps lock behavior not being disabled
416dff1260b8844bc1ec11cf5f127d5a537b2031 introduced an issue where
the dconf config was masking/shadowing the `xserver.xkb.options` config.

Resolves #72

configuration.nix
@@ -48,7 +48,9 @@
 
     excludePackages = [ pkgs.xterm ];
 
-    xkb.options = "ctrl:nocaps";
+    # If xkbOptions are set in dconf config, they shadow this. Do not do.
+    # https://discourse.nixos.org/t/setting-caps-lock-as-ctrl-not-working/11952
+    xkb.options = "compose:ralt,ctrl:nocaps";
   };
 
   services.gnome.core-utilities.enable = false;
@@ -185,7 +187,8 @@
     dconf.settings = with lib.hm.gvariant; {
       "org/gnome/desktop/input-sources" = {
         sources = [ (mkTuple [ "xkb" "us" ]) ];
-        xkb-options = [ "compose:ralt" ];
+
+        # Do not set xkb-options here. They will shadow the xserver.xkb.options config
       };
       "org/gnome/desktop/interface" = {
         color-scheme = "prefer-dark";