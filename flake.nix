0f16b37fde107abdfdd51c79b0cbd338b8301272
ignormies <bryce.beagle@gmail.com>
2025-06-28 19:01:30 -0700

    Start using `nixos-hardware`
    Also did a `nix flake update`
    
    Resolves #171

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     # https://discourse.nixos.org/t/differences-between-nix-channels/13998
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    nixos-hardware.url = "github:nixos/nixos-hardware/master";
     impermanence.url = "github:nix-community/impermanence";
 
     nix-colors.url = "github:misterio77/nix-colors";
@@ -28,12 +29,14 @@
   outputs = {
     self,
     nixpkgs,
+    nixos-hardware,
     ...
   } @ inputs: {
     nixosConfigurations = {
       poundcake = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [
+          nixos-hardware.nixosModules.framework-16-7040-amd
           ./hardware-configuration.nix
           ./configuration.nix
         ];
