8d963584d66ba1f7ce8329de08852737b0f3ee2c
ignormies <bryce.beagle@gmail.com>
2025-07-07 16:29:51 -0700

    Set up `neovim` cursor smearing
    Result is far from perfect, but I think it's still an improvement
    
    Resolves #176

--- neovim/navigation.nix
+++ neovim/navigation.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.nixvim = {
 
     opts = {
@@ -64,5 +64,43 @@
         };
       };
     };
+
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "smear-cursor-nvim";
+        src = builtins.fetchGit {
+          url = "https://github.com/sphamba/smear-cursor.nvim";
+          ref = "refs/tags/v0.4.7";
+          rev = "aff844fc1483fd673f721a41affcd7e2fcb885f5";
+        };
+      })
+    ];
+
+    extraConfigLuaPost = ''
+      require("smear_cursor").setup({
+        -- Disable smearing in insert mode. Visually distracting; cursor moves less.
+        smear_insert_mode = false,
+
+        -- Increase head speed, reduce tail speed
+        stiffness = 0.5,
+        trailing_stiffness = 0.3,
+
+        -- Always render smear, no matter the distance
+        max_length = 999,
+
+        -- Increase framerate
+        time_interval = 7,
+
+        -- Default smear color does not seems to respect actual cursor color
+        -- (Likely related to change for https://github.com/BryceBeagle/nixos-config/issues/177)
+        -- I also kinda like the red. Note that this does not appear as true red,
+        -- (I guess because of theming?), and more as a pastel red.
+        cursor_color = "#ff0000",
+
+        -- Use better symbols
+        legacy_computing_symbols_support = true,
+        legacy_computing_symbols_support_vertical_bars = true,
+      })
+    '';
   };
 }
