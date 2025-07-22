3a85d5505d7cd14b3ac415df12c68f65113dcef4
ignormies <bryce.beagle@gmail.com>
2025-07-22 15:40:19 -0700

    Add `colorful-menu.nvim` to autocompletion menus
    Resolves #200

--- neovim/completion.nix
+++ neovim/completion.nix
@@ -1,6 +1,8 @@
-{ ... }: {
+{ config, ... }: {
   programs.nixvim = {
     plugins = {
+      colorful-menu.enable = true;
+
       blink-cmp = {
         enable = true;
 
@@ -13,6 +15,26 @@
             # Consider full tokens even if cursor is in the center of one
             # https://cmp.saghen.dev/configuration/reference.html#completion-keyword
             keyword.range = "full";
+
+            # Using default blink<->colorful-menu configuration as documented here:
+            # https://github.com/xzbdmw/colorful-menu.nvim?tab=readme-ov-file#use-it-in-blinkcmp
+            #
+            # Ideally, there would be a dedicated option for this
+            menu.draw = {
+              columns = config.lib.nixvim.utils.mkRaw ''
+                { { "kind_icon" }, { "label", gap = 1 } }
+              '';
+              components = {
+                label = {
+                  text = config.lib.nixvim.utils.mkRaw ''
+                    require("colorful-menu").blink_components_text
+                  '';
+                  highlight = config.lib.nixvim.utils.mkRaw ''
+                    require("colorful-menu").blink_components_highlight
+                  '';
+                };
+              };
+            };
           };
         };
       };
