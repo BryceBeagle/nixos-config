b7eafc82d946d8f15e3838bc1008f5d5ec96af1f
ignormies <bryce.beagle@gmail.com>
2025-07-21 19:46:21 -0700

    Lower delay-on-failure for incorrect `gdm` passwords
    Resolves #196

--- security.nix
+++ security.nix
@@ -6,12 +6,17 @@
     # Shorten delay-on-failure from 3 seconds to 0.5 seconds.
     # Technically this is less secure, but I'm not going to worry about a 6x
     # speed-up for brute forces.
-    pam.services.sudo = {
-      nodelay = true;
-      failDelay = {
-        enable = true;
-        delay = 500000;
+    pam.services = let
+      shortenFailDelay = {
+        nodelay = true;
+        failDelay = {
+          enable = true;
+          delay = 500000;
+         };
       };
+    in {
+      login = shortenFailDelay;
+      sudo = shortenFailDelay;
     };
   };
 
