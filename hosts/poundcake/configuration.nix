54623e32d2d241fd7e362dde942bb43f1d37bcb0
ignormies <bryce.beagle@gmail.com>
2025-08-06 19:21:33 -0700

    Migrate rest of terminal configuration to `denix`
    Going to split up this `terminal` module in the future, but this will
    work for now.
    
    Resolves: #234

--- hosts/poundcake/configuration.nix
+++ hosts/poundcake/configuration.nix
@@ -19,23 +19,9 @@
     ];
   };
 
-  console.font = "Lat2-Terminus16";
-
   # Globally enabled for all users
   environment.systemPackages = with pkgs; [
     # CLI tools
-    curl
-    ghostty
-    jq
-    just
-    libinput
-    neovim
-    ripgrep
-    tree
-    unzip
-    usbutils
-    wget
-
     # Useful for LSPs and other file-editing
     cargo
     rust-analyzer
@@ -48,15 +34,4 @@
     ];
 
   programs.git.enable = true;
-
-  programs.zsh.enable = true;
-  programs.fish.enable = true;
-
-  # Default shell for users (i.e. in a terminal emulator)
-  users.defaultUserShell = pkgs.fish;
-
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-  };
 }
--- modules/terminal.nix
+++ modules/terminal.nix
@@ -1,4 +1,8 @@
-{delib, ...}:
+{
+  delib,
+  pkgs,
+  ...
+}:
 delib.module {
   name = "terminal";
 
@@ -11,28 +15,65 @@ delib.module {
     ".local/share/fish"
   ];
 
-  home.ifEnabled.programs.direnv = {
-    enable = true;
+  # Default terminal configuration for other users
+  nixos.ifEnabled = {
+    # TTY font
+    console.font = "Lat2-Terminus16";
 
-    silent = true;
-  };
+    # Default shell for all users
+    users.defaultUserShell = pkgs.fish;
 
-  home.ifEnabled.programs.ghostty = {
-    enable = true;
+    # Other assorted, globally installed packages
+    environment.systemPackages = with pkgs; [
+      curl
+      jq
+      just
+      libinput
+      ripgrep
+      tree
+      unzip
+      usbutils
+      wget
+    ];
 
-    settings = {
-      cursor-invert-fg-bg = true;
-      theme = "catppuccin-macchiato";
-      window-decoration = "none";
+    programs = {
+      fish.enable = true;
+
+      # Backup if fish isn't working for some reason
+      zsh.enable = true;
+
+      neovim = {
+        enable = true;
+
+        defaultEditor = true;
+      };
     };
   };
 
-  home.ifEnabled.programs.fish = {
-    enable = true;
+  home.ifEnabled.programs = {
+    direnv = {
+      enable = true;
 
-    functions = {
-      # Disable "Welcome to fish" message
-      fish_greeting = "";
+      silent = true;
+    };
+
+    fish = {
+      enable = true;
+
+      functions = {
+        # Disable "Welcome to fish" message
+        fish_greeting = "";
+      };
+    };
+
+    ghostty = {
+      enable = true;
+
+      settings = {
+        cursor-invert-fg-bg = true;
+        theme = "catppuccin-macchiato";
+        window-decoration = "none";
+      };
     };
   };
 }
