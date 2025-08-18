fd417ebe8d2f0b56ef13387522ea29c780b07c64
ignormies <bryce.beagle@datadoghq.com>
2025-08-17 17:43:27 -0700

    Fix borderless `ghostty` windows on MacOS
    `window-decoration` only works on Linux. It causes very buggy behavior
    on MacOS.
    
    Resolves: #256

--- modules/programs/terminal/ghostty.nix
+++ modules/programs/terminal/ghostty.nix
@@ -1,4 +1,9 @@
-{delib, ...}:
+{
+  delib,
+  lib,
+  pkgs,
+  ...
+}:
 delib.module {
   name = "programs.terminal.ghostty";
 
@@ -18,7 +23,11 @@ delib.module {
     settings = {
       cursor-invert-fg-bg = true;
       theme = "catppuccin-macchiato";
-      window-decoration = "none";
+
+      # TODO: denix doesn't provide a way to do linux- or darwin-only home config
+      # TODO: Also, consider moving this to desktop-environment settings
+      window-decoration = lib.mkIf pkgs.stdenv.isLinux "none";
+      macos-titlebar-style = lib.mkIf pkgs.stdenv.isDarwin "hidden";
     };
   };
 }
