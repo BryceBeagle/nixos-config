b178a69c55fcd8f59f78c129a197382a8bf53053
ignormies <bryce.beagle@gmail.com>
2025-12-14 21:47:18 -0800

    Disable Force Click on MacOS
    The intent of this is to disable dictionary/lookup on Force Click.
    I have no other use for Force Click.
    
    Resolves: #300

--- modules/config/touchpad.nix
+++ modules/config/touchpad.nix
@@ -6,6 +6,14 @@
 delib.module {
   name = "touchpad";
 
+  darwin.always.system.defaults.NSGlobalDomain = {
+    # Completely disable Force Click. This is the only way I can find to turn off the
+    # annoying dictionary/lookup utility when force clicking words. I have never
+    # encountered another thing that uses Force Click, so completely disabling this
+    # doesn't bother me.
+    "com.apple.trackpad.forceClick" = false;
+  };
+
   home.always = {myconfig, ...}:
     lib.mkIf myconfig.desktop-environment.gnome.enable {
       dconf.settings = {
