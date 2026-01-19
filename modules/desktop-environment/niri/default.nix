44d321cfe2a61ec9452ac6ed3a1b0d5e44a05f8d
ignormies <bryce.beagle@gmail.com>
2026-01-19 00:11:44 -0800

    Add `xwayland-satellite` for X11 compat in `niri`
    Resolves: #322

--- modules/desktop-environment/niri/default.nix
+++ modules/desktop-environment/niri/default.nix
@@ -13,11 +13,19 @@ delib.module {
     inputs.niri.nixosModules.niri
   ];
 
-  nixos.ifEnabled.programs.niri = {
-    enable = true;
+  nixos.ifEnabled = {
+    # niri will use xwayland-satellite if it finds it on the PATH
+    # This is used to launch/run applications that only run on X11
+    environment.systemPackages = with pkgs; [
+      xwayland-satellite
+    ];
 
-    # Use upstream package instead of the random binary cached with the flake
-    # https://github.com/sodiboo/niri-flake#binary-cache
-    package = pkgs.niri;
+    programs.niri = {
+      enable = true;
+
+      # Use upstream package instead of the random binary cached with the flake
+      # https://github.com/sodiboo/niri-flake#binary-cache
+      package = pkgs.niri;
+    };
   };
 }
