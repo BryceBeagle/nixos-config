ba2dadc9cf1cb6bb6460b8e32fc4a2bc1f342ac6
ignormies <bryce.beagle@gmail.com>
2026-03-10 20:34:37 -0700

    Add corner radii to `niri` windows
    Resolves: #355

--- modules/theming/windows.nix
+++ modules/theming/windows.nix
@@ -8,7 +8,25 @@ delib.module {
 
   home.ifEnabled = {myconfig, ...}: {
     programs = lib.optionalAttrs myconfig.desktop-environment.niri.enable {
-      niri.settings.prefer-no-csd = true;
+      niri.settings = {
+        prefer-no-csd = true;
+
+        window-rules = [
+          {
+            clip-to-geometry = true;
+
+            # For some reason, niri itself allows setting this to a single int/float,
+            # if all values are the same, but the niri flake requires it to be a full
+            # attrset of floats
+            geometry-corner-radius = {
+              bottom-left = 18.0;
+              bottom-right = 18.0;
+              top-left = 18.0;
+              top-right = 18.0;
+            };
+          }
+        ];
+      };
     };
   };
 }
