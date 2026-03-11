32ef078c2000226bd4cc2be7398f7cd5775965e5
ignormies <bryce.beagle@gmail.com>
2026-03-10 20:18:09 -0700

    Use `catppuccin-macchiato` theme built into `fish`
    Resolves: #354

--- modules/theming/color.nix
+++ modules/theming/color.nix
@@ -12,16 +12,27 @@ delib.module {
       base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
 
       targets = {
-        # Chooses a base16 theme by default, even though we can easily use a base24 theme
-        nixvim.enable = false;
+        # stylix uses base16-fish, which does not have any catppuccin themes
+        # https://github.com/BryceBeagle/nixos-config/issues/354
+        #
+        # But fish has one built-in:
+        # https://github.com/fish-shell/fish-shell/blob/master/share/themes/catppuccin-macchiato.theme
+        fish.enable = false;
 
         # Has more limited colors
         # https://github.com/BryceBeagle/nixos-config/issues/336#issuecomment-4028988354
         ghostty.enable = false;
+
+        # Chooses a base16 theme by default, even though we can easily use a base24 theme
+        nixvim.enable = false;
       };
     };
 
     programs = {
+      fish.shellInit = ''
+        fish_config theme choose "catppuccin-macchiato"
+      '';
+
       ghostty = lib.mkIf myconfig.programs.terminal.ghostty.enable {
         settings.theme = "Catppuccin Macchiato";
       };
