97419a8c21567c45e9d43007ceb3d15cc19b19a9
ignormies <bryce.beagle@gmail.com>
2026-03-10 23:50:16 -0700

    Fix `cd -` with `cd && ls` wrapper
    We want our `cd` to wrap the `cd` wrapper.
    
    Resolves: #356

--- modules/programs/terminal/navigation.nix
+++ modules/programs/terminal/navigation.nix
@@ -9,19 +9,20 @@ in
 
     home.ifEnabled = {myconfig, ...}: {
       programs.fish = lib.mkIf myconfig.programs.terminal.fish.enable {
-        functions = {
-          # Always ls/eza after cd-ing.
-          # Note: this uses the eza alias defined in ./filesystem.nix
-          cd = {
-            body = ''
-              builtin cd $argv
-              or return
-
-              eza
-            '';
-            wraps = "cd";
-          };
-        };
+        # Always ls/eza after cd-ing.
+        #
+        # Note: this uses the eza alias defined in ./filesystem.nix
+        #
+        # Note: we use shellInit here with the weird 'functions -c' because
+        # cd is _already_ a wrapper function, and we want to wrap the wrapper.
+        # See: https://github.com/BryceBeagle/nixos-config/issues/356
+        shellInit = ''
+          functions -c cd standard_cd
+          function cd --wraps cd
+              standard_cd $argv
+              and eza
+          end
+        '';
       };
     };
   }
