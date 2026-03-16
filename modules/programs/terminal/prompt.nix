151dc7e7c58776b35a96c5b6c57f12e017e4cb87
ignormies <bryce.beagle@gmail.com>
2026-03-16 14:46:34 -0700

    Add `git` branch to `fish` prompt if in repo
    Was really missing this from the built-in prompt.
    
    Resolves: #353

--- modules/programs/terminal/prompt.nix
+++ modules/programs/terminal/prompt.nix
@@ -9,24 +9,35 @@ delib.module {
   home.ifEnabled = {myconfig, ...}: {
     programs.fish = lib.mkIf myconfig.programs.terminal.fish.enable {
       functions.fish_prompt = ''
+        set -l git_branch_icon   # Nerd Fonts nf-dev-git_branch e725
+
         set -l last_status $status
 
         set -l color_normal (set_color normal)
+        set -l color_branch (set_color $fish_color_end)
         set -l color_cwd (set_color $fish_color_cwd)
         set -l color_user (set_color $fish_color_user)
+
+        set -l user $color_user $USER $color_normal
+        set -l cwd $color_cwd (prompt_pwd) $color_normal
+
+        set -l maybe_branch
+        if set branch_name (git branch --show-current 2>/dev/null)
+            set -l branch $git_branch_icon " " $branch_name
+            set maybe_branch " " $color_branch $branch $color_normal
+        else
+            set maybe_branch ""
+        end
 
         set -l color_dot
         if test $last_status -eq 0
             set color_dot (set_color $fish_color_end)
         else
-            set color_dot (set_color $fish_color_error)
+            set color_dot (set_color $fish_color_status)
         end
         set -l dot $color_dot "▪" $color_normal
 
-        set -l user $color_user $USER $color_normal
-        set -l cwd $color_cwd (prompt_pwd) $color_normal
-
-        string join "" -- '╭─(' $user ')(' $cwd ')'
+        string join "" -- '╭─(' $user ')(' $cwd ')' $maybe_branch
         string join "" -- '╰──' $dot ' '
       '';
     };
