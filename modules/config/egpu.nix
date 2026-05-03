0b5643525eb1788fa6daf39fe31c8c82b4b04f1e
ignormies <bryce.beagle@gmail.com>
2026-05-02 20:49:23 -0700

    Get eGPU working with `niri`
    Resolves: #319

--- modules/config/egpu.nix
+++ modules/config/egpu.nix
@@ -17,18 +17,44 @@ delib.module {
     cfg,
     myconfig,
     ...
-  }:
-    lib.mkIf myconfig.desktop-environment.gnome.enable {
+  }: {
+    services.udev.extraRules = lib.mkMerge [
+      # Symlink the eGPU to standardized path so it can be used by arbitrary tools
+      (
+        lib.concatStrings [
+          ''SUBSYSTEM=="drm", ''
+          ''ACTION=="add", ''
+          ''ATTRS{vendor}=="${cfg.vendorId}", ''
+          ''ATTRS{device}=="${cfg.deviceId}", ''
+          ''SYMLINK+="dri/by-name/egpu"''
+        ]
+      )
       # Tell Gnome/mutter to use the eGPU as the "primary" GPU
       # https://gitlab.gnome.org/GNOME/mutter/-/blob/main/doc/multi-gpu.md
-      services.udev.extraRules = lib.concatStrings [
-        ''SUBSYSTEM=="drm", ''
-        ''ENV{DEVTYPE}=="drm_minor", ''
-        ''ENV{DEVNAME}=="/dev/dri/card[0-9]", ''
-        ''SUBSYSTEMS=="pci", ''
-        ''ATTRS{vendor}=="${cfg.vendorId}", ''
-        ''ATTRS{device}=="${cfg.deviceId}", ''
-        ''TAG+="mutter-device-preferred-primary"''
-      ];
+      (
+        lib.mkIf myconfig.desktop-environment.gnome.enable (
+          lib.concatStrings [
+            ''SUBSYSTEM=="drm", ''
+            ''ENV{DEVTYPE}=="drm_minor", ''
+            ''ENV{DEVNAME}=="/dev/dri/card[0-9]", ''
+            ''SUBSYSTEMS=="pci", ''
+            ''ATTRS{vendor}=="${cfg.vendorId}", ''
+            ''ATTRS{device}=="${cfg.deviceId}", ''
+            ''TAG+="mutter-device-preferred-primary"''
+          ]
+        )
+      )
+    ];
+  };
+
+  home.ifEnabled = {myconfig, ...}: {
+    programs = lib.optionalAttrs myconfig.desktop-environment.niri.enable {
+      niri.settings = {
+        # Use the symlink created above as the preference. If this symlink is missing,
+        # niri appears to be fine falling back to whatever it would have otherwise
+        # chosen
+        debug.render-drm-device = "/dev/dri/by-name/egpu";
+      };
     };
+  };
 }
